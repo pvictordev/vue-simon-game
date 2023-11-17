045d1cadf6d6826b6fa22923dcd59177e1cde4cb
Purice Victor <pvictor0110@gmail.com>
2023-11-17 12:25:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-simon-game',
+  base: '/vue-simon-game/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
